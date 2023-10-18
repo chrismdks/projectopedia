f8d3e99baec968418b774ea422b48689e4ceb392
chrismdks <chris.mdks@gmail.com>
2023-10-18 20:12:08 +0300

    configured routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
 import Header from './Components/layout/Header'
 import Footer from './Components/layout/Footer'
 import Home from './Pages/Home'
+import TopicsCovered from './Pages/TopicsCovered'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
-      <Header/>
-      <Home/>
-      <Footer/>
+      <BrowserRouter>
+        <Header/>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/topicscovered' element={<TopicsCovered/>}></Route>
+          </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   )
 }
--- src/Components/layout/Header.jsx
+++ src/Components/layout/Header.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import logo from '../../images/react.png'
 import React from 'react'
 
@@ -19,21 +20,20 @@ function Header() {
               aria-expanded="false" 
               aria-label="Toggle navigation">
               <span className="navbar-toggler-icon">
-
               </span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="#">
+                  <NavLink className="nav-link" aria-current="page" to="/">
                     Home
-                  </a>
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Link
-                  </a>
+                  <NavLink className="nav-link" to="/topicscovered">
+                    Topics
+                  </NavLink>
                 </li>
               </ul>
             </div>
