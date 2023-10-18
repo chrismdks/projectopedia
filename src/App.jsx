910c3329ccd0eac5b2613bb49d24fa5e00c73174
chrismdks <chris.mdks@gmail.com>
2023-10-18 12:38:56 +0300

    set up basic components of project - Header,Footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/layout/Header'
+import Footer from './Components/layout/Footer'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
